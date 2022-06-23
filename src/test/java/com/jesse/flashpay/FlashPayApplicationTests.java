35cb6ca5ffdbd9c52ee1466045f35a8680148971
JesseYuan <1831232665@qq.com>
2022-06-23 14:29:23 +0800

    first commit

---
+++ src/test/java/com/jesse/flashpay/FlashPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jesse.flashpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
